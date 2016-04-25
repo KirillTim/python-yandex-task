f330512a76ce61a71f4eef119fbd4f9dbaf0173b
Kirill <timofeev.kirill.a@gmail.com>
2016-04-25 19:45:36 +0300

    read bytes instead of strings

--- first.py
+++ first.py
@@ -3,14 +3,15 @@ if len(sys.argv) < 2:
     print("./first <file name>")
     exit(1)
 
-with open(sys.argv[1]) as f:
+with open(sys.argv[1], "rb") as f:
     files = {}
     sender = None
+    FROM_BYTES = str.encode("From ")
     for line in f:
-        if line.startswith("From "):
-            sender = line.split(' ')[1]
+        if line[:5] == FROM_BYTES:
+            sender = line.decode().split(' ')[1]
             if sender not in files:
-                files[sender] = open(sender, "w")
+                files[sender] = open(sender, "wb")
         if sender:
             files[sender].write(line)
 
