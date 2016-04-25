99d32cfc3c436e1de43440bdcccd49f7f28912ba
Kirill <timofeev.kirill.a@gmail.com>
2016-04-25 19:54:07 +0300

    pep8 style fixes

--- first.py
+++ first.py
@@ -1,19 +1,26 @@
 import sys
-if len(sys.argv) < 2:
-    print("./first <file name>")
-    exit(1)
 
-with open(sys.argv[1], "rb") as f:
-    files = {}
-    sender = None
-    FROM_BYTES = str.encode("From ")
-    for line in f:
-        if line[:5] == FROM_BYTES:
-            sender = line.decode().split(' ')[1]
-            if sender not in files:
-                files[sender] = open(sender, "wb")
-        if sender:
-            files[sender].write(line)
 
-    for f in files.values():
-        f.close()
+def main():
+    if len(sys.argv) < 2:
+        print("./first <file name>")
+        exit(1)
+
+    with open(sys.argv[1], "rb") as input:
+        from_bytes = str.encode("From ")
+        files = {}
+        sender = None
+        for line in input:
+            if line[:5] == from_bytes:
+                sender = line.decode().split(' ')[1]
+                if sender not in files:
+                    files[sender] = open(sender, "wb")
+            if sender:
+                files[sender].write(line)
+
+        for f in files.values():
+            f.close()
+
+
+if __name__ == "main":
+    main()
