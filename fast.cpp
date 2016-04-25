9031ae8697dea754bd8dae045cf1a93932282724
Kirill <timofeev.kirill.a@gmail.com>
2016-04-25 20:35:35 +0300

    print newline char after each line, lol

--- fast.cpp
+++ fast.cpp
@@ -12,19 +12,18 @@ int main(int argc, char** argv) {
     string cur;
     const string from = "From ";
     string mail = "";
+    bool have = false;
     while (getline(in, cur)) {
         if (cur.substr(0, from.size()) == from) {
             int fi = cur.find(' ');
             int se = cur.find(' ', fi+1);
             mail = cur.substr(fi+1, se-fi-1);
-           // cerr<<"mail = "<<mail<<"\n";
             if (out.find(mail) == out.end()) {
                 out.insert(make_pair(mail, ofstream(mail)));
-                //cerr <<"add record, out.sz= "<<out.size()<<"\n";
             }
         }
         if (!mail.empty())
-            out[mail] << cur;
+            out[mail] << cur<<"\n";
     }
     for (auto& i : out)
         i.second.close();
