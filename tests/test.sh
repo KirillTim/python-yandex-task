5832ae237c5b7e285f1ec44a8eb7445680cc65e1
Kirill <timofeev.kirill.a@gmail.com>
2016-04-25 20:47:20 +0300

    unicode tests add

---
+++ tests/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+function check {
+    touch result
+    cat auth1@china.com >> result 
+    cat auth2@japan.com >> result
+    cat auth3@arab.com >> result
+    diff result unicode.mailbox
+    if [ $? != 0 ]; then
+        echo "files aren't equal"
+    else
+        echo "OK"
+    fi
+}
+
+rm auth1@china.com auth2@japan.com auth3@arab.com result
+python3 ../first.py unicode.mailbox
+check
+rm auth1@china.com auth2@japan.com auth3@arab.com result
+python3 ../first.py unicode.mailbox
+check
