1d87a15399e4af7b2590d6721daffe152d0bcf16
Kirill <timofeev.kirill.a@gmail.com>
2016-04-25 19:59:03 +0300

    pep8

--- test_gen.py
+++ test_gen.py
@@ -1,22 +1,26 @@
 import sys
-line = [''.join([c] * 1000) for c in ["a", "b"]]
+
 
 def gen(senders_count, lines_per_mail, mails_per_sender):
+    line = [''.join([c] * 1000) for c in ["a", "b"]]
     with open("maxtest.mailbox", "w") as f:
-        senders = ["sender"+str(x)+"@mail.com" for x in range(senders_count)]
+        senders = ["sender" + str(x) + "@mail.com" for x in range(senders_count)]
         for i in range(mails_per_sender):
             for s in senders:
-                msg = "From "+s+" Any Date Here\n"
+                msg = "From " + s + " Any Date Here\n"
                 for j in range(lines_per_mail):
-                    msg += (line[j % 2]+"\n")
-                msg += "\n";
-                f.write(msg);
+                    msg += (line[j % 2] + "\n")
+                msg += "\n"
+                f.write(msg)
+
 
 def main():
-    if (len(sys.argv) < 4):
+    if len(sys.argv) < 4:
         print("test_gen.py <senders_count> <lines_per_mail> <mails_per_sender>")
         exit(1)
     else:
-        gen(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]));
+        gen(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
 
-main()
+
+if __name__ == "main":
+    main()
