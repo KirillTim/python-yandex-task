23c647c3373eb9322d9a2ef04798ff635eb8dd10
Kirill Timofeev <timofeev.kirill.a@gmail.com>
2016-04-26 23:02:23 +0300

    more pip8 style fixes

--- kirill_timofeev.py
+++ kirill_timofeev.py
@@ -1,30 +1,33 @@
-#tests and benchmarks: https://github.com/KirillTim/python-yandex-task
+# tests and benchmarks: https://github.com/KirillTim/python-yandex-task
 import sys
+
+READ_BUF_SIZE = 1024*1024  # in bytes
+FROM_BYTES = str.encode("From ")
 
 
 def main():
     if len(sys.argv) < 2:
         print(sys.argv[0]+" <file name>")
         exit(1)
-    #sometimes such a big read buffer can help
-    #avoid python encoding troubles -- read bytes, not strings
-    with open(sys.argv[1], "rb", buffering = 1024*1024) as input:
-        from_bytes = str.encode("From ") #it's 2016, everybody should use utf-8
-        #it works with other encodings though
+    # sometimes such a big read buffer can help
+    # avoid python encoding troubles -- read bytes, not strings
+    with open(sys.argv[1], "rb", buffering=READ_BUF_SIZE) as file:
+        # it's 2016, everybody should use utf-8
+        # it works with other encodings though
         files = {}
         sender = prev_sender = None
         buf = bytearray()
-        for line in input:
-            if line[:5] == from_bytes:
+        for line in file:
+            if line[:len(FROM_BYTES)] == FROM_BYTES:
                 sender = line.decode().split(' ')[1]
                 if sender not in files:
                     files[sender] = open(sender, "wb")
-            if sender: #skip all lines before first mail header
+            if sender:  # skip all lines before first mail header
                 if not prev_sender:
                     prev_sender = sender
                 elif sender != prev_sender:
-                    #write all email using one write() syscall
-                    #python is smart enough to set the size of write() buffer as len(buf)
+                    # write all email using one write() syscall. python is
+                    # smart to set the size of write() buffer equal to len(buf)
                     files[prev_sender].write(buf)
                     prev_sender = sender
                     buf = bytearray()
