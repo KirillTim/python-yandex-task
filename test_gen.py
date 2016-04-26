dcef2eb6bcc93d3ddeb9468a782a09213e26bc4f
Kirill Timofeev <timofeev.kirill.a@gmail.com>
2016-04-26 23:05:15 +0300

    more pep8 style fixes

--- test_gen.py
+++ test_gen.py
@@ -4,7 +4,7 @@ import sys
 def gen(senders_count, mails_per_sender, lines_per_mail, chars_per_line):
     line = [''.join([c] * chars_per_line) for c in ["a", "b"]]
     with open("maxtest.mailbox", "w") as f:
-        senders = ["sender" + str(x) + "@mail.com" for x in range(senders_count)]
+        senders = ["sender"+str(x)+"@mail.com" for x in range(senders_count)]
         for i in range(mails_per_sender):
             for s in senders:
                 msg = "From " + s + " Any Date Here\n"
@@ -16,10 +16,12 @@ def gen(senders_count, mails_per_sender, lines_per_mail, chars_per_line):
 
 def main():
     if len(sys.argv) < 5:
-        print(sys.argv[0]+" <senders_count> <mails_per_sender> <lines_per_mail> <chars_per_line>")
+        print(sys.argv[0]+" <senders_count> <mails_per_sender> "
+                          "<lines_per_mail> <chars_per_line>")
         exit(1)
     else:
-        gen(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
+        gen(int(sys.argv[1]), int(sys.argv[2]),
+            int(sys.argv[3]), int(sys.argv[4]))
 
 
 if __name__ == "__main__":
