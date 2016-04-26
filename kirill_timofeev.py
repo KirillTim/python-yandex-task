3c365980cab19f681c8a20124e9bd0296748b8ea
Kirill Timofeev <timofeev.kirill.a@gmail.com>
2016-04-26 22:09:12 +0300

    add comments

--- kirill_timofeev.py
+++ kirill_timofeev.py
@@ -6,9 +6,11 @@ def main():
     if len(sys.argv) < 2:
         print(sys.argv[0]+" <file name>")
         exit(1)
-
-    with open(sys.argv[1], "rb") as input:
-        from_bytes = str.encode("From ")
+    #sometimes such a big read buffer can help
+    #avoid python encoding troubles -- read bytes, not strings
+    with open(sys.argv[1], "rb", buffering = 1024*1024) as input:
+        from_bytes = str.encode("From ") #it's 2016, everybody should use utf-8
+        #it works with other encodings though
         files = {}
         sender = prev_sender = None
         buf = bytearray()
@@ -17,10 +19,12 @@ def main():
                 sender = line.decode().split(' ')[1]
                 if sender not in files:
                     files[sender] = open(sender, "wb")
-            if sender:
+            if sender: #skip all lines before first mail header
                 if not prev_sender:
                     prev_sender = sender
                 elif sender != prev_sender:
+                    #write all email using one write() syscall
+                    #python is smart enough to set the size of write() buffer as len(buf)
                     files[prev_sender].write(buf)
                     prev_sender = sender
                     buf = bytearray()
